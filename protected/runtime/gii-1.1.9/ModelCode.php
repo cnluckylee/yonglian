df21654c7a42a585b27f555d24ce967207388d58
cnluckylee@gmail.com <cnluckylee@gmail.com@0eed56f3-c458-7fa0-5cad-6b2300b0600e>
2012-07-04 10:01:37 +0000

    创建项目，完成用户登录验证

---
+++ protected/runtime/gii-1.1.9/ModelCode.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'template' => 'default',
+  'tablePrefix' => 'yl_users',
+  'modelPath' => 'application.models',
+  'baseClass' => 'CActiveRecord',
+  'buildRelations' => '1',
+);
