94ca4f9259c4c58abee0ef5449e0b34ec3246cc2
cnluckylee@gmail.com <cnluckylee@gmail.com@0eed56f3-c458-7fa0-5cad-6b2300b0600e>
2012-07-04 10:01:37 +0000

    创建项目，完成用户登录验证

---
+++ trunk/protected/views/site/pages/about.php
@@ -0,0 +1,10 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - 关于';
+$this->breadcrumbs=array(
+	'关于',
+);
+?>
+<h1>About</h1>
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <tt><?php echo __FILE__; ?></tt>.</p>
