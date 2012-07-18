aa343702833f642092da3af16382a819ac085e1c
cnluckylee@gmail.com <cnluckylee@gmail.com@0eed56f3-c458-7fa0-5cad-6b2300b0600e>
2012-07-18 15:06:35 +0000

    后台行业设置提交，并修复查询bug

---
+++ trunk/protected/modules/Admin/views/adminIndustry/view.php
@@ -0,0 +1,25 @@
+<?php
+$this->breadcrumbs=array(
+	'Admin Industries'=>array('index'),
+	$model->name,
+);
+
+$this->menu=array(
+	array('label'=>'List adminIndustry', 'url'=>array('index')),
+	array('label'=>'Create adminIndustry', 'url'=>array('create')),
+	array('label'=>'Update adminIndustry', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete adminIndustry', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage adminIndustry', 'url'=>array('admin')),
+);
+?>
+
+<h1>View adminIndustry #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'name',
+		'remark',
+	),
+)); ?>
